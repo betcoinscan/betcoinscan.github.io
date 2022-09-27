7274368ae97ef2e9744cd4a485e63fd1b58e1e27
Sintaklaas <sklaas034@gmail.com>
2022-09-27 16:42:52 +0200

    Community bets

--- js/script.js
+++ js/script.js
@@ -28,6 +28,29 @@ var options = {minimumFractionDigits: 0, maximumFractionDigits: 5};
 var tokenprice = new Intl.NumberFormat(locale, options);
 
 $( document ).ready(function() {
+
+    getPickOfTheDay();
+
+    $.getJSON('https://api.betcoinscan.com/buyback.php',function(data){
+
+        $('.buybacktotal').html('<i class="bet"></i>'+formatter.format(data.buyback))
+        $('.betprice').html(tokenprice.format(data.price))
+        $('.marketcap').html(formatter.format(data.price*100000000))
+
+        if(data.change.substr(0,1)=='-') {
+            $('.pricechange').html('<span class="down">' + data.change + '</span>');
+        }else{
+            $('.pricechange').html('<span class="up">' + data.change + '</span>');
+        }
+
+        $('.buybackpercentage').html(formatter2.format(data.percentage)+'%');
+        $('.buybackvalue').html(formatter.format(data.value));
+
+    });
+});
+
+function getPickOfTheDay(){
+    $('.betlist').html('');
     $.getJSON('https://api.betcoinscan.com/',function(data){
 
         $('.netreturn').html(formatter.format(data.stats.netresult));
@@ -55,36 +78,77 @@ $( document ).ready(function() {
 
             var betHTML = '<tr>' +
                 '<td>'+bet.Date+'</td>' +
-                    '<td>'+bet.League+'</td>' +
-                    '<td class="pick-of-the-day">' +
-                    '<span class="bet"><a href="'+bet['Link']+'" target="_blank">'+bet['Pick of the day']+'</a></span>' +
-                    '<span class="dd">'+bet['Game']+'</span>' +
-                    '</td>' +
-                    '<td>'+bet.Dime+'</td>' +
-                    '<td>'+formatter.format(bet.Allocation)+'</td>' +
-                    '<td>'+formatter.format(bet['Net Return'])+'</td>' +
-                    '<td>'+bet.Odds+'</td>' +
-                    '<td class="'+classn+'">'+text+'</td>' +
+                '<td>'+bet.League+'</td>' +
+                '<td class="pick-of-the-day">' +
+                '<span class="bet"><a href="'+bet['Link']+'" target="_blank">'+bet['Pick of the day']+'</a></span>' +
+                '<span class="dd">'+bet['Game']+'</span>' +
+                '</td>' +
+                '<td>'+bet.Dime+'</td>' +
+                '<td>'+formatter.format(bet.Allocation)+'</td>' +
+                '<td>'+formatter.format(bet['Net Return'])+'</td>' +
+                '<td>'+bet.Odds+'</td>' +
+                '<td class="'+classn+'">'+text+'</td>' +
                 '</tr>';
-            $('.betlist').append(betHTML);
+            $('.betlist.bet1').append(betHTML);
 
         }
     });
+}
 
-    $.getJSON('https://api.betcoinscan.com/buyback.php',function(data){
+function getCommunityBets(){
+    $('.betlist').html('');
+    $.getJSON('https://api.betcoinscan.com/community.php',function(data){
 
-        $('.buybacktotal').html('<i class="bet"></i>'+formatter.format(data.buyback))
-        $('.betprice').html(tokenprice.format(data.price))
-        $('.marketcap').html(formatter.format(data.price*100000000))
+        /*$('.netreturn').html(formatter.format(data.stats.netresult));
+        $('.totalwon').html(formatter.format(data.stats.profit));
+        $('.totallost').html(formatter.format(data.stats.loss));
+        $('.wonamount').html(data.stats.won);
+        $('.lostamount').html(data.stats.lost);
+        $('.pushamount').html(data.stats.push);
+        $('.winrate').html(data.stats.winrate+'%');
+*/
+        for(b in data.bets){
+            var bet = data.bets[b];
 
-        if(data.change.substr(0,1)=='-') {
-            $('.pricechange').html('<span class="down">' + data.change + '</span>');
-        }else{
-            $('.pricechange').html('<span class="up">' + data.change + '</span>');
+            if(bet.Won==1) {
+                var classn = 'won';
+                var text = 'Won';
+                if (bet.Won == 0) {
+                    var classn = 'lost';
+                    var text = 'Lost';
+                }
+
+                if (typeof (bet['Livescore']) !== 'undefined' && bet['Livescore'] !== '') {
+                    var classn = '';
+                    var text = '<a href="' + bet['Livescore'] + '" target="_blank" class="espn"></a>';
+                }
+
+                var betHTML = '<tr>' +
+                    '<td>' + bet.Date + '</td>' +
+                    '<td>' + bet.League + '</td>' +
+                    '<td class="pick-of-the-day">' +
+                    '<span class="bet"><a href="' + bet['Link'] + '" target="_blank">' + bet['Pick of the day'] + '</a></span>' +
+                    '<span class="dd">' + bet['Game'] + '</span>' +
+                    '</td>' +
+                    '<td>' + bet.Dime + '</td>' +
+                    '<td>' + formatter.format(bet.Allocation) + '</td>' +
+                    '<td>' + formatter.format(bet['Net Return']) + '</td>' +
+                    '<td>' + bet.Odds + '</td>' +
+                    '<td class="' + classn + '">' + text + '</td>' +
+                    '</tr>';
+                $('.betlist.bet2').append(betHTML);
+            }
+
         }
-
-        $('.buybackpercentage').html(formatter2.format(data.percentage)+'%');
-        $('.buybackvalue').html(formatter.format(data.value));
-
     });
-});
+}
+function showTab(which){
+
+    if(which==1) getPickOfTheDay();
+    else if(which==2) getCommunityBets();
+    $('a.tab').removeClass('active');
+    $('a.tab'+which).addClass('active');
+
+    $('div.table').hide();
+    $('div.table'+which).show();
+}
